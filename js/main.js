7020b2e413224fac5122b5f4fdbbfebe4eb808c9
chrisborneman <cborneman@gmail.com>
2015-01-02 01:25:20 -0500

    changed css

--- js/main.js
+++ js/main.js
@@ -46,9 +46,9 @@ $('#refialink a').click
       }
       else if($('refiabattles').is(":visible"))
       {
-        $('#battles').slideToggle("slow", function()
+        $('#battles').slideToggle(0, function()
           {
-            $('#refiabattles').toggle();
+            $('#refiabattles').slideToggle("slow");
           }
         );     
       }
@@ -56,7 +56,7 @@ $('#refialink a').click
       {
         $('#battles').slideToggle(0, function()
           {
-            $('#refiabattles').slideToggle();
+            $('#refiabattles').slideToggle("slow");
           }
         );
       }
