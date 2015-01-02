5f322b321c5d2a72ab986779cbe7a33c8928c45e
chrisborneman <cborneman@gmail.com>
2015-01-01 21:14:00 -0500

    changed css

--- js/main.js
+++ js/main.js
@@ -4,11 +4,19 @@ $('#fwlink a').click
     {
       if($('#refiabattles').is(":visible"))
       {
+        $('#refialink a').css("color","#FFF0D5");
         $('#battles').toggle();
         $('#refiabattles').toggle();
       }
+      else if($('#fwbattles').is(":visible"))
+      {
+        $('#fwlink a').css("color","#FFF0D5");
+        $('#battles').toggle();
+        $('#fwbattles').toggle();
+      }
       else
       {
+        $('#fwlink a').css("color","#AD3149");
         $('#battles').toggle();
         $('#fwbattles').toggle();
       }
@@ -21,11 +29,19 @@ $('#refialink a').click
     {
       if($('#fwbattles').is(":visible"))
       {
+        $('#fwlink a').css("color","#FFF0D5");
         $('#battles').toggle();
         $('#fwbattles').toggle();
       }
+      else if($('#refiabattles').is(":visible"))
+      {
+        $('#refialink a').css("color","#FFF0D5");
+        $('#battles').toggle();
+        $('#refiabattles').toggle();
+      }
       else
       {
+        $('#refialink a').css("color","#1E97BF");
         $('#battles').toggle();
         $('#refiabattles').toggle();
       }
