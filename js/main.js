f663c11b7c912c37678be43fd7cb6a278b91f9e0
chrisborneman <cborneman@gmail.com>
2015-01-01 22:08:46 -0500

    changed css

--- js/main.js
+++ js/main.js
@@ -5,20 +5,20 @@ $('#fwlink a').click
       if($('#refiabattles').is(":visible"))
       {
         $('#refialink a').css("color","#FFF0D5");
-        $('#battles').toggle();
-        $('#refiabattles').toggle();
+        $('#battles').slideToggle();
+        $('#refiabattles').slideToggle();
       }
       else if($('#fwbattles').is(":visible"))
       {
         $('#fwlink a').css("color","#FFF0D5");
-        $('#battles').toggle();
-        $('#fwbattles').toggle();
+        $('#battles').slideToggle();
+        $('#fwbattles').slideToggle();
       }
       else
       {
         $('#fwlink a').css("color","#AD3149");
-        $('#battles').toggle();
-        $('#fwbattles').toggle();
+        $('#battles').slideToggle();
+        $('#fwbattles').slideToggle();
       }
     }
   )
@@ -30,20 +30,20 @@ $('#refialink a').click
       if($('#fwbattles').is(":visible"))
       {
         $('#fwlink a').css("color","#FFF0D5");
-        $('#battles').toggle();
-        $('#fwbattles').toggle();
+        $('#battles').slideToggle();
+        $('#fwbattles').slideToggle();
       }
       else if($('#refiabattles').is(":visible"))
       {
         $('#refialink a').css("color","#FFF0D5");
-        $('#battles').toggle();
-        $('#refiabattles').toggle();
+        $('#battles').slideToggle();
+        $('#refiabattles').slideToggle();
       }
       else
       {
         $('#refialink a').css("color","#1E97BF");
-        $('#battles').toggle();
-        $('#refiabattles').toggle();
+        $('#battles').slideToggle();
+        $('#refiabattles').slideToggle();
       }
     }
   )
